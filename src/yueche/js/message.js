05d00cbd3482dbe14f0533ec060a2916013a8734
Your Name <you@example.com>
2018-03-27 23:33:13 +0800

    可以旋转

---
+++ src/yueche/js/message.js
@@ -0,0 +1,95 @@
+$(function () {
+    //上传图片
+    $(".fileUpload").on("change", function (e) {
+        layer.open({
+            type: 2,
+            content: '上传中...',
+            shadeClose: false
+        });
+        var file = e.target.files[0];
+        var Orientation = null;
+        // if (!file.type.match('image.*')) {
+        //     return false;
+        // }
+        EXIF.getData(file, function () {
+            Orientation = EXIF.getTag(this, 'Orientation');
+        });
+
+        var fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        fileReader.onload = function (event) {
+            var result = event.target.result; //返回的dataURL
+            var image = new Image();
+            image.src = result;
+            image.onload = function () { //创建一个image对象，给canvas绘制使用
+
+                var cvs = document.createElement('canvas');
+                var ctx = cvs.getContext('2d');
+                var scale = 1; //预留压缩比
+
+                cvs.width = this.width * scale;
+                cvs.height = this.height * scale; //计算等比缩小后图片宽高
+
+                if (Orientation && Orientation != 1) {
+                    switch (Orientation) {
+                        case 6: // 旋转90度
+
+                            cvs.width = this.height * scale;
+                            cvs.height = this.width * scale;
+                            ctx.rotate(Math.PI / 2);
+                            // (0,-imgHeight) 从旋转原理图那里获得的起始点
+                            ctx.drawImage(this, 0, -this.height * scale, this.width * scale, this.height * scale);
+                            break;
+                        case 3: // 旋转180度
+                            ctx.rotate(Math.PI);
+                            ctx.drawImage(this, this.width * scale, -this.height * scale, this.width * scale, this.height * scale);
+                            break;
+                        case 8: // 旋转-90度
+
+                            cvs.width = this.height * scale;
+                            cvs.height = this.width * scale;
+                            ctx.rotate(3 * Math.PI / 2);
+                            ctx.drawImage(this, -this.width * scale, 0, this.width * scale, this.height * scale);
+                            break;
+
+                    }
+                } else {
+                    ctx.drawImage(this, 0, 0, cvs.width, cvs.height);
+                }
+                // 更换模板
+                $('.avatarImg').attr('src', cvs.toDataURL('image/png', 0.7));
+                layer.closeAll();
+            };
+        };
+    });
+
+    var pinchRotateImg = $('.avatarImg')[0];
+
+    var mobanImg = $(".fileUpload")[0];
+    Transform(pinchRotateImg);
+    new AlloyFinger(mobanImg, {
+        rotate: function (evt) {
+            pinchRotateImg.rotateZ += evt.angle;
+        },
+        multipointStart: function () {
+            initScale = pinchRotateImg.scaleX;
+        },
+        pinch: function (evt) {
+            pinchRotateImg.scaleX = pinchRotateImg.scaleY = initScale * evt.zoom;
+        },
+        pressMove: function (evt) {
+            pinchRotateImg.translateX += evt.deltaX;
+            pinchRotateImg.translateY += evt.deltaY;
+            evt.preventDefault();
+        }
+    });
+
+    $(".submit").click(function(){
+        html2canvas(document.querySelector("#avatar"), {
+            backgroundColor: 'transparent', // 设置背景透明
+        }).then(function (canvas) {
+            $("#avatarResult").attr("src", canvas.toDataURL("image/png"));
+        });
+    });
+
+});
